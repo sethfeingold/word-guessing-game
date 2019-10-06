b9c519c68acbf55ca83d365d3f92257a729ae4b4
SethFeingold <sfeingold1104@gmail.com>
2019-10-06 11:54:47 -0700

    main.js linked, start button functionality implemented

--- js/main.js
+++ js/main.js
@@ -1,2 +1,9 @@
-let keyboard = document.getElementById('qwerty');
-let phrase = document.getElementById('phrase');
+const keyboard = document.getElementById('qwerty');
+const phrase = document.getElementById('phrase');
+let missed = 0;
+const overlay = document.getElementById('overlay');
+const startButton = overlay.getElementsByClassName('btn__reset')[0];
+
+startButton.addEventListener('click', () => {
+    overlay.style.display = 'none';
+});
