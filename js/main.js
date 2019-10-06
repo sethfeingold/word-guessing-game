7da1823cb2b8f5c105b9e0930c9f4c414faba4de
SethFeingold <sfeingold1104@gmail.com>
2019-10-06 13:01:45 -0700

    addPhraseToDisplay function
    adds randomly selected phrase to the game display and applys class of 'letter' to any character that isn't a space

--- js/main.js
+++ js/main.js
@@ -7,6 +7,7 @@ const phrase = document.getElementById('phrase');
 let missed = 0;
 const overlay = document.getElementById('overlay');
 const startButton = overlay.getElementsByClassName('btn__reset')[0];
+const phraseUL = phrase.firstElementChild;
 
 
 /* *********** //
@@ -45,3 +46,23 @@ function getRandomPhraseAsArray(arr) {
     //return array of new characters
     return newCharacterArray;
 };
+
+// Store result of getRandomPhraseAsArray
+let gamePhrase = getRandomPhraseAsArray(phraseList);
+
+// Add Phrase to Display Function
+
+function addPhraseToDisplay(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        let phraseLI = document.createElement('li');
+        phraseLI.innerText = arr[i];
+
+        if (phraseLI.innerText !== " ") {
+            phraseLI.className = 'letter';
+        }
+
+        phraseUL.appendChild(phraseLI);
+    }
+};
+
+addPhraseToDisplay(gamePhrase);
