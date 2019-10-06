119fc45ff32c7c80d97054bb655445ba02697d70
SethFeingold <sfeingold1104@gmail.com>
2019-10-06 11:58:19 -0700

    adding comments to js for clarity

--- js/main.js
+++ js/main.js
@@ -1,9 +1,20 @@
+/* *********** //
+    Variables
+// *********** */
+
 const keyboard = document.getElementById('qwerty');
 const phrase = document.getElementById('phrase');
 let missed = 0;
 const overlay = document.getElementById('overlay');
 const startButton = overlay.getElementsByClassName('btn__reset')[0];
 
+/* *********** //
+   ~~ Magic ~~
+// *********** */
+
+//hide overlay when start button is clicked
+
 startButton.addEventListener('click', () => {
     overlay.style.display = 'none';
 });
+
